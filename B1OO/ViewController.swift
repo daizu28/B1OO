7ad140ca571a4bd31b872ffc10dd65da0bbd731b
Reina Kobayashi <kinakodaizukinako@gmail.com>
2020-09-29 15:54:26 +0900

    test

---
+++ B1OO/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  B1OO
+//
+//  Created by 学生用 on 2020/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var test: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
